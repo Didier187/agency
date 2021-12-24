e4f6c54e7954e8c3a31e58ab4feee4ae1b1abb8e
Didier <Kdondidier@gmail.com>
2021-12-24 16:30:55 +1100

    some code

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // javascript
-const menuBtn = document.getElementById('menu-icon')
-const sideNav = document.getElementById('side-nav-overlay')
+const menuBtn = document.getElementById("menu-icon");
+const sideNav = document.getElementById("side-nav-overlay");
+const humbergers = document.getElementsByClassName("humberger");
 
-menuBtn.addEventListener('click', ()=> {
-    sideNav.classList.toggle('show-side-nav')
-})
-sideNav.addEventListener('click',()=>{
-    sideNav.classList.toggle('show-side-nav')
-})
+menuBtn.addEventListener("click", () => {
+  sideNav.classList.toggle("show-side-nav");
+  for (let humberItem of humbergers) {
+    humberItem.classList.toggle("turn-green");
+  }
+  if (sideNav.classList.contains("show-side-nav") && window.innerWidth <= 600) {
+    document.body.style.overflow = "hidden";
+  }else{
+    document.body.style.overflow = "visible"
+  }
+});
+
+sideNav.addEventListener("click", () => {
+  sideNav.classList.toggle("show-side-nav");
+});
